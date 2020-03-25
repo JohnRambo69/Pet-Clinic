7bab36f61a8e81b9f152eb7f08b70c96fb34e863
JohnRambo69 <50015050+JohnRambo69@users.noreply.github.com>
2020-03-25 16:44:21 +0100

    added SpecialityMap impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.springframework.petclinic.services;
+
+import com.springframework.petclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
