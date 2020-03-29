3274afcab89e2e189205674623f5661a1d86e363
JohnRambo69 <50015050+JohnRambo69@users.noreply.github.com>
2020-03-29 17:21:55 +0200

    last commit for now.

--- pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/PetController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
 import java.util.Collection;
 
 @Controller
@@ -30,6 +32,18 @@ public class PetController {
         this.petService = petService;
         this.ownerService = ownerService;
         this.petTypeService = petTypeService;
+    }
+
+    @InitBinder
+    public void dataBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+
+        dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException{
+                setValue(LocalDate.parse(text));
+            }
+        });
     }
 
     @ModelAttribute("types")
@@ -63,9 +77,10 @@ public class PetController {
         }
         owner.getPets().add(pet);
         if (result.hasErrors()) {
-            model.put("pet", pet);
+            model.addAttribute("pet", pet);
             return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
         } else {
+            pet.setOwner(owner);
             petService.save(pet);
 
             return "redirect:/owners/" + owner.getId();
@@ -85,7 +100,7 @@ public class PetController {
             model.addAttribute("pet", pet);
             return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
         } else {
-            owner.getPets().add(pet);
+            pet.setOwner(owner);
             petService.save(pet);
             return "redirect:/owners/" + owner.getId();
         }
